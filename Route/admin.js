2a6ff1fab5f7a0f9388c5263d8e46c0b93fe1b26
ripu <damanripu664@gmail.com>
2020-10-17 02:15:13 +0530

    DataBase Added

--- Route/admin.js
+++ Route/admin.js
@@ -1,8 +1,38 @@
 const express = require("express")
 const router = new express.Router()
-
+const Admin = require("../models/adminModel")
 router.get("/admin", (req, res) => {
     res.render("admin")
+})
+router.post("/admin", (req, res) => {
+    const adminID = req.body.adminId
+    const password = req.body.password
+    console.log(adminID)
+        // console.log(password)
+    if (!adminID || !password) {
+        res.render("admin", {
+            message: "*Missing parameters"
+        })
+    } else {
+        const admin = Admin.findOne({ email: adminID }, (err, op) => {
+            if (password == op.password) {
+                if (op) {
+                    res.send(`user found`)
+
+                } else {
+                    res.render("admin", {
+                        message: "Invalid user "
+                    })
+                }
+            } else {
+                res.render("admin", {
+                    message: "Invalid Password"
+                })
+            }
+        })
+
+    }
+
 })
 
 module.exports = router
--- Route/adminReg.js
+++ Route/adminReg.js
@@ -1,11 +1,38 @@
 require("dotenv").config()
 const express = require("express")
 const router = new express.Router()
-
+const Admin = require("../models/adminModel")
 router.get("/adminReg", (req, res) => {
     res.render("adminReg")
 })
 router.post("/adminReg", (req, res) => {
-    res.send("Recived")
+    const email = req.body.email
+    const secretCode = req.body.secretCode
+    const password = req.body.password
+    const cpassword = req.body.cpassword
+    if (!email || !secretCode || !password || !password) {
+        res.send("Missing Data")
+    } else {
+        if (secretCode == process.env.SECRET) {
+            if (password == cpassword) {
+                const admin = new Admin({
+                    email: email,
+                    password: password
+                })
+                admin.save().then(() => {
+                    res.redirect("/admin")
+                })
+            } else {
+                res.render("adminReg", {
+                    message: "Password do not match"
+                })
+            }
+        } else {
+            res.render("adminReg", {
+                message: "Invalid Secret Code"
+            })
+        }
+    }
+
 })
 module.exports = router
