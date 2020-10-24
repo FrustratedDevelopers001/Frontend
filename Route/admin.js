0e698a84bccdcc9ce0dc16afe3b25e25ba435766
ripu <damanripu664@gmail.com>
2020-10-25 02:22:03 +0530

    Added Automated Email Service

--- Route/admin.js
+++ Route/admin.js
@@ -1,11 +1,23 @@
 require("dotenv").config()
 const express = require("express")
-const bcrypt = require("bcrypt")
+const nodeMailer = require("nodemailer")
 const router = new express.Router()
 const Notice = require("../models/noticeModel")
 const Admin = require("../models/adminModel")
 const Student = require("../models/studentModel")
 var nametobeSent = ""
+
+
+const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MAIL_ID,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
+
+
+
 const getName = (name) => {
     nametobeSent = name
 }
@@ -95,10 +107,63 @@ router.post("/admin/student/add", async(req, res) => {
         await student.save()
         res.redirect("/admin/adminPanel")
     } catch (error) {
+        console.log(error)
         res.render("AddStudent", {
-            message: "Student already added"
+            message: "*Student already added"
         })
     }
+    try {
+        const info = await transporter.sendMail({
+            from: process.env.MAIL_ID,
+            to: student.studentEmail,
+            subject: `${student.department} deparartment registration`,
+
+            html: `<body>
+
+            <h2>Your registration for ${student.academicSession} is sucssfull</h1>
+            <div>
+            
+                <h3>Following are the list of subject and sections alloted to ${student.firstName.toUpperCase(  )} ${student.lastName.toUpperCase()} bearing enrollment number ${student.enrollment} </h3>
+                <tr>
+                    <th style="margin-left: 1em; !important">subject</th>
+                    <th style="margin-left: 1em; !important">Section</th>
+                    <th style="margin-left: 1em; !important">Faculty</th>
+                </tr>
+                <tr>
+                    <td>${student.subjects[0].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[0].section}</td>
+                </tr>
+                <tr>
+                    <td>${student.subjects[1].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[1].section}</td>
+                </tr>
+                <tr>
+                    <td>${student.subjects[2].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[2].section}</td>
+                </tr>
+                <tr>
+                    <td>${student.subjects[3].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[3].section}</td>
+                </tr>
+                <tr>
+                    <td>${student.subjects[4].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[4].section}</td>
+                </tr>
+                <tr>
+                    <td>${student.subjects[5].subject}</td>
+                    <td style="margin-left: 1em;"> ${student.subjects[5].section}</td>
+                </tr>
+                
+            </div>
+        </body>`
+
+        })
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
 
 })
 
